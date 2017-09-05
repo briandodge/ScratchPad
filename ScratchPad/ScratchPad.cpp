57c9dc7e9adce9d1a5d2c3c49a1083cd12048518
Brian Dodge <direfuldodger85@live.co.uk>
2017-09-05 19:55:19 +0100

    added pointers

--- ScratchPad/ScratchPad.cpp
+++ ScratchPad/ScratchPad.cpp
@@ -6,9 +6,33 @@
 
 //namespace std
 
+
+void swap(int *num1, int *num2) {
+
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+
 int main()
 {
 	std::cout << "hello" << std::endl;
+	int number1, number2;
+
+	number1 = 10;
+	number2 = 15;
+
+	std::cout << "Number 1: " << number1 << std::endl;
+	std::cout << "Number 2: " << number2 << std::endl;
+
+	std::cout << "Swap..." << std::endl;
+
+	swap(&number1, &number2);
+	
+	std::cout << "Number 1: " << number1 << std::endl;
+	std::cout << "Number 2: " << number2 << std::endl;
+
     return 0;
 }
 
