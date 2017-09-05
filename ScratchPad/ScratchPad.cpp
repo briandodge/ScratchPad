c9c69e85133bd289b74ea7363084471258f96d2f
Brian Dodge <Brian Dodge>
2017-09-05 19:29:16 +0100

    Add project files.

---
+++ ScratchPad/ScratchPad.cpp
@@ -0,0 +1,14 @@
+// ScratchPad.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+//namespace std
+
+int main()
+{
+	std::cout << "hello" << endl;
+    return 0;
+}
+
